49c9dc4e131f28881d55e584e0f9466591bd54c4
Dragonixj <delafiado@gmail.com>
2023-11-29 10:12:24 +0000

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Installing flask from pip
-
-package {
-    ensure => '2.1.0',
-    provider => 'pip3',
-}
--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-exec { 'Append Line IdentityFile':
-  command => '/bin/echo -e "IdentityFile ~/.ssh/school\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
-  path    => '/etc/ssh/ssh_config',
-}
