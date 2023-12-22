7f9b2129784cd6f54bd3a3efdc378a8ae7432026
Dragonixj <delafiado@gmail.com>
2023-12-22 22:51:19 +0000

    made some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,21 @@
 #installing flask from pip3
 
-package { 'python3-pip':
-    ensure  =>  installed,
+package { 'python3.8':
+  ensure => present,
 }
 
-exec { 'install_flask':
-    command =>  '/usr/bin/pip3 install flask==2.1.0',
-    unless  =>  '/usr/bin/pip3 show flask | grep Version | grep -q "2.1.0"',
-    require =>  Package['python3-pip'],
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
