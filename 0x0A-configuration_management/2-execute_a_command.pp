6c692b1c1e312a2d30d658523246d454ef23e606
Dragonixj <delafiado@gmail.com>
2023-12-22 22:37:57 +0000

    made some changes

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Kill the process named "killmenow" using pkill
+
+exec { 'killmenow_process':
+    command =>  '/usr/bin/pkill -f killmenow',
+    refreshonly =>  true,
+    onlyif  =>  '/usr/bin/pgrep -f killmenow',
+}
