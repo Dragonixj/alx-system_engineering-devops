d2dc5ba4ca4d81ec1e2af56517ee3a3522132f85
Dragonixj <delafiado@gmail.com>
2024-02-14 11:59:43 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,16 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers to the subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     sub_info = get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
+        url,
         headers={"User-Agent": "My-User-Agent"},
         allow_redirects=False,
     )
 
-    if sub_info.status_code >= 300:
+    if sub_info.status_code != 200:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    data = sub_info.json().get("data")
+    if data is None:
+        return 0
+    return data.get("subscribers", 0)
