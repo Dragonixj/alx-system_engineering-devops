ee08f8709e50cc16cb389a6306b42172f064062a
Dragonixj <delafiado@gmail.com>
2024-02-16 18:34:21 +0000

    fixing errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Queries the reddit api and returns number of suscribers"""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the title of the top 10 hottest posts on a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    sub_top_ten = requests.get(
-        url,
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
-        params={"limit": 10},
-    )
-
-    if sub_top_ten.status_code == 404:
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
         print("None")
         return
-    results = sub_top_ten.json().get("data")
-    [print(child.get("data").get("title")) for child in results.get("children")]
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
